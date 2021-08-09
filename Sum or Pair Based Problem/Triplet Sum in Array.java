f85a4a924ae118dd94cc42808e046684bb119cd5
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-08-09 11:20:45 +0530

    Update Triplet Sum in Array.java

--- Sum or Pair Based Problem/Triplet Sum in Array.java
+++ Sum or Pair Based Problem/Triplet Sum in Array.java
@@ -1,3 +1,29 @@
+
+/*TC=>O(N^3)
+SC=>O(1)*/
+class Solution
+{
+    public static boolean find3Numbers(int A[], int n, int X) { 
+        for(int i=0;i<n-2;i++)
+        {
+            for(int j=i+1;j<n-1;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    if(A[i]+A[j]+A[k]==X)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/*TC=>O(N^2)
+SC->O(1)*/
+
 class Solution
 {
     public static boolean find3Numbers(int A[], int n, int X) { 
@@ -18,12 +44,19 @@ class Solution
                 }
                 else
                 {
-                    j++;
+                  j++;  
                 }
             }
         }
         return false;
-        
-    
     }
 }
+
+
+
+
+
+
+
+
+
